1705ab9b04ef38d673e4978eac106fd7c9a79210
Vinh Tran <vinhtran2905@gmail.com>
2018-04-27 13:06:52 -0500

    Handling Alert by switchTo and Alert element

---
+++ src/TestScripts/JavaScriptPopUp.java
@@ -0,0 +1,31 @@
+package TestScripts;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptPopUp {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.name("proceed")).click();
+		
+		//pop up appears, action: get text of pop up and click button OK
+		
+		//switch from screen to pop up window
+		Alert alert = driver.switchTo().alert();
+		
+		Thread.sleep(5000);
+		System.out.println(alert.getText());
+		alert.accept(); // click on OK button
+		//alert.dismiss();//click on Cancel button
+		
+		
+		
+		
+		
+	}
+}
